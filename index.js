c9125826a94c5df78281176c3818d1225aeebb9d
Tausif Ahmed <tausifahmed471@gmail.com>
2022-02-18 08:10:51 +0600

    recap

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ function sayHello (){
                  }
   
   saySomething("rahim",29);
+
+  class Vehicle {
+  
+    myVar = "yes";
+   constructor(param,count){
+     this.color =  param;
+     this.passenger = count;
+     
+   }
+   
+   saySomething(){
+     
+     console.log(this.color);
+   }
+ }
+ 
+ let object1 = new Vehicle("red",25);
+ let object2 = new Vehicle("blue",69);
+ 
+ console.log(object1);
+ console.log(object2);
+ 
+ object1.saySomething();
+ object2.saySomething();
+
+ /// inheritance ->
+ 
