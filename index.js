7b16c148062f678c75989dfcf342f4caf49e9490
Tausif Ahmed <tausifahmed471@gmail.com>
2022-02-18 08:34:49 +0600

    destructing recap

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-let numbers = [1,2,3,4,5,6];
-let numbers2 = [7,8,9,0,3465];
+// destructing -->>
 
- let arr = [...numbers ,...numbers2,69,96];
+let arr = [1,5,19];
 
+let [a,,b] = arr; // -> if we want to skip the number 5 , we need to use ,, in between of the two variables <-
 
- let gamers = {
-     ign: "markeloff",
-     age: 69
- }
-
- let moreGamers = {...gamers, rank: "supreme"};
-
- console.log(moreGamers);
-
-// rest -> used to merge -> 
-
-let something = (...numarr) => console.log(numarr);
-something(1,2,3,4,5,6,7,8,9,76,6,6,435353453535); // rest operator does the same for objects.
+console.log(a,b);
