54d89224877308ccbac08aca7e5465f3ee7d6e9f
Tausif Ahmed <tausifahmed471@gmail.com>
2022-02-18 07:59:40 +0600

    moving on

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("randomizing modules");
+function sayHello(){
+    console.log("markeloff");
+}
+
+function sayHello (){
+    console.log("markelof");
+  }
+  
+  sayHello();
+  
+  // arrow
+  
+  let saySomething = (name,age) => 
+                 {
+                   console.log(name,age);
+                 }
+  
+  saySomething("rahim",29);
